73f6762f7d60e15f8558d83252210d97a8869399
boogiepopo <ari.lappalainen@student.laurea.fi>
2017-07-10 23:09:38 +0300

    Test.c
    -

---
+++ test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+
+void limit(int stop);
+int verify(int check);
+int main()
+{
+    int x;
+
+    printf("Enter a stopping value (0-100):");
+    scanf("%d", &x);
+    if(verify(x))
+    {
+        limit(x);
+    }
+    else
+    {
+        printf("%d is out of range!\n");
+    }
+    return 0;
+}
+int verify(int check)
+{
+    if(check < 0 || check > 100)
+        return FALSE;
+    return TRUE;
+}
+void limit(int stop)
+{
+    int x;
+    for(x=0;x<=100;x=x+1)
+    {
+        printf("%d",x);
+        if(x==stop)
+        {
+            puts("You won!");
+            return;
+        }
+    }
+    puts("I won!");
+}
